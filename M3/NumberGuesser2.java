593c53d1227a8c7c05b43e29e3b82b74071db413
mkiss28 <98507798+mkiss28@users.noreply.github.com>
2023-02-12 18:28:00 -0500

    finished the hw

--- M3/NumberGuesser2.java
+++ M3/NumberGuesser2.java
@@ -4,6 +4,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuesser2 {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         // there's a lot of repeated code and it's not the cleanest. We'll see later how
         // to improve this
@@ -16,7 +19,9 @@ public class NumberGuesser2 {
             int strikes = 0;
             int maxStrikes = 5;
             int number = -1;
+            int hintsused = 1;
             boolean pickNewRandom = true;
+
             do {
                 if (pickNewRandom) {
                     int range = 9 + ((level - 1) * 5);
@@ -34,6 +39,34 @@ public class NumberGuesser2 {
                     System.out.println("Tired of playing? No problem, see you next time.");
                     break;
                 }
+
+                //6. Add a hint command that can be used once per level and only after 2 strikes have been used that reduces the range around the correct number (i.e., number is 5 and range is initially 1-15, new range could be 3-8 as a hint)
+                
+                //mbk28 2/12/2023
+                
+                if (message.equalsIgnoreCase("hint")) {
+
+                    if(hintsused > 1){
+
+                        System.out.println("You've used your hints for this level");
+
+                    }else if (strikes < 2) {
+                        
+                        System.out.println("You neeed to use 2 strike before you can use a hint");
+                          
+                    
+                    }else{
+
+                        hintsused++;
+
+                        int hintrange = 9 + ((level - 1) * 5);
+                        
+                        System.out.println("the number is between " + (number / 2) + " and " + (((hintrange + number)/2)+1));
+
+                    }
+
+                }
+
                 int guess = -1;// default value
                 try {
                     // inner catch to lazily check if the entered value is a number
@@ -49,6 +82,7 @@ public class NumberGuesser2 {
                         System.out.println("That's right!");
                         level++;// level up!
                         strikes = 0;// make sure we clear these out, otherwise it's not really fair
+                        hintsused = 1;
                         pickNewRandom = true;
 
                     } else {
@@ -63,8 +97,26 @@ public class NumberGuesser2 {
                                 level = 1;
                             }
                             pickNewRandom = true;
+
+                        
                         }
+                        //1. Display higher or lower as a hint after a wrong guess
+
+                        //mbk28 2/12/2023
+                        
+                        else if (guess > number ){
+
+                            System.out.println("your guess is too high");
+
+                        } else if(guess < number){
+
+                            System.out.println("your guess is too low");
+
+                        }
+
+
                     }
+
                 }
             } while (true);
         } catch (Exception e) {
